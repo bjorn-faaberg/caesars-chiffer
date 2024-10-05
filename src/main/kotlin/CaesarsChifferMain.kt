5166b8bbaae2dd3eaed83170279ce0ad29887073
Bjørn Faaberg <bjorn.faaberg@mattilsynet.no>
2024-10-05 11:53:55 +0200

    Accepts parameters for launch with some validation

--- src/main/kotlin/CaesarsChifferMain.kt
+++ src/main/kotlin/CaesarsChifferMain.kt
@@ -1,12 +1,46 @@
 import io.quarkus.runtime.QuarkusApplication
 import io.quarkus.runtime.annotations.QuarkusMain
-
+import java.io.File
 
 @QuarkusMain
-class HelloWorldMain : QuarkusApplication {
-    @Throws(Exception::class)
+class CaesarsChifferMain : QuarkusApplication {
     override fun run(vararg args: String): Int {
-        println("Hello " + args[0])
+        var decode = false
+        var inputFile: String? = null
+        var outputFile: String? = null
+        var shiftCount: Int? = null
+
+        when (args.size) {
+            5, 6 -> {
+                for (i in args.indices) {
+                    when (args[i]) {
+                        "--decode" -> decode = true
+                        "--inputfile" -> inputFile = args.getOrNull(i + 1)
+                        "--outputfile" -> outputFile = args.getOrNull(i + 1)
+                        "--shiftcount" -> shiftCount = args.getOrNull(i + 1)?.toIntOrNull()
+                    }
+                }
+            }
+
+            else -> {
+                parameterValidationOutput()
+                println("Usage: --inputfile <inputfile> --outputfile <outputfile> [--decode]")
+                return 1
+            }
+        }
+        if (shiftCount == null || inputFile == null || outputFile == null || (decode && args.size == 5)) {
+            parameterValidationOutput()
+            return 1
+        } else {
+            val file = File(inputFile)
+            if (!file.exists()) {
+                println("inputfile $inputFile not found")
+                return 1
+            }
+        }
         return 0
     }
+
+    private fun parameterValidationOutput() =
+        println("Usage: --shiftcount <shiftcount as integer> --inputfile <inputfile> --outputfile <outputfile> [--decode]")
 }
